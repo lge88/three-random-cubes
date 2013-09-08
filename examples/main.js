8da69a59dbb12f75b86f76243d292bad20298bb6
Li Ge <lge@ucsd.edu>
2013-09-08 04:57:56 -0700

    Update at Sun Sep  8 04:57:56 PDT 2013

--- examples/main.js
+++ examples/main.js
@@ -4,7 +4,7 @@ var randomCubes = require( 'three-random-cubes' );
 var viewport = ISEViewport();
 var scene = viewport.scene;
 var cubes = randomCubes(
-  100,
+  50,
   [-200, 200],
   [-300, 300],
   [-100, 100]
